5c4ce7fb18cb079d541507740bc95dd045bd1b44
zEustace <115109332+zEustace@users.noreply.github.com>
2024-08-17 17:28:56 -0300

    Adding tests & updating endpoint

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def create_task():
     task_id_control +=1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "New task created"})
+    return jsonify({"message": "New task created", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
